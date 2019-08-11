899339bab00c7ff2fa1cbcf2d0b87c721aabee8e
David Kevork <davidkevork@users.noreply.github.com>
2019-08-12 09:20:08 +1000

    Create q1_a.js

---
+++ COSC2536/assignment_1/q1_a.js
@@ -0,0 +1,14 @@
+const ciphertext = '';
+
+// Decode Caesars Cipher
+
+for (let i = 0; i < 26; i++) {
+  let plaintextForward = '';
+  let plaintextBackward = '';
+  for (let j = 0; j < ciphertext.length; j++) {
+    plaintextForward += String.fromCharCode(ciphertext.charCodeAt(j) + i);
+    plaintextBackward += String.fromCharCode(ciphertext.charCodeAt(j) - i);
+  }
+  console.log(i + 1, plaintextForward, plaintextBackward);
+}
+
