cfb6d69a952969f439712ead1ad8ab2475111083
David Kevork <davidkevork@users.noreply.github.com>
2019-08-16 16:27:55 +1000

    Update q2.js

--- COSC2536/assignment_1/q2.js
+++ COSC2536/assignment_1/q2.js
@@ -9,12 +9,13 @@ function sha256 (buffer) {
 const alice = '73DAA9289DDD08A53BA86F065DDB07BF915ABA208BEC652E999613D2A8444228';
 const bob = '9197E4844ABED2FEA3569A2ACF7B0D584C979C333AB7AE10BA6C339898776F5A';
 
-for (let i = 351; i < 365; i++) {
+for (let i = 351; i < 366; i++) {
   const hash = sha256(i.toString()).toString('hex');
+  console.log(`h(${i}): ${hash}`);
   if (hash === alice.toLowerCase()) {
-    console.log(`Alice bid found: ${i} - ${hash}`);
+    console.log(`Alice's bid found: ${i} - ${hash}`);
   }
   if (hash === bob.toLowerCase()) {
-    console.log(`Bob bid found: ${i} - ${hash}`);
+    console.log(`Bob's bid found: ${i} - ${hash}`);
   }
 }
