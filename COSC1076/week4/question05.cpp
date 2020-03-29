1c8b87f28e88355a6af525946255b4cd436fa16c
David Kevork <davidkevork@users.noreply.github.com>
2020-03-29 11:54:41 +1100

    Create question05.cpp

---
+++ COSC1076/week4/question05.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void doubleArray(int values[], int length);
+
+int main() {
+  int length = 10;
+  int values[length];
+  for (int i = 0; i < length; i++) {
+    values[i] = i;
+  }
+  doubleArray(values, length);
+  for (int i = 0; i < length; i++) {
+    std::cout << "values[" << i << "]: " << values[i] << std::endl;
+  }
+}
+
+void doubleArray(int values[], int length) {
+  for (int i = 0; i < length; i++) {
+    values[i] *= 2;
+  }
+}
