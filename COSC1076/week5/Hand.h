e8c742fd81863485663b7dc2f5bce0e601e7f524
David Kevork <davidkevork@users.noreply.github.com>
2020-03-30 10:05:39 +1100

    Create Hand.h

---
+++ COSC1076/week5/Hand.h
@@ -0,0 +1,23 @@
+#ifndef HAND_H
+#define HAND_H
+
+#include "Card.h"
+
+#define MAX_CARDS 5
+
+class Hand {
+public:
+    Hand();
+    Hand(Hand& other);
+    ~Hand();
+    
+    void addCard(Card* card);
+    int getNumCards();
+    Card* getCard(int index);
+
+private:
+    Card* cards[MAX_CARDS];
+    int numStored;
+};
+
+#endif // HAND_H
